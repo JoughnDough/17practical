f5408b64ad26de4ec4e49e2de28935b14fff9284
JoughnDough <4574790@myuwc.ac.za>
2026-03-25 14:07:34 +0200

    Implemented insert algorithm
    Implemented the algorithm that enters numbers of 2^n - 1 in a sequence dictated by halving the numbers again and again on either side until they are all inserted

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 
 
 public class Main {
+
+    private static void insertNum(int start, int end) {
+        int middle = (start + end) / 2;
+
+        System.out.println("Inserted: " + middle);
+
+        if (start != end) {
+            System.out.println("[" + start + "," + (middle - 1) + "] | [" + (middle + 1) + "," + end + "]");
+            insertNum(start, middle - 1);
+            insertNum(middle + 1, end);
+        }
+    }
+
+    private static void insertNum(int n) {
+        int maxNum = Math.max((1 << n) - 1, 1);
+
+        insertNum(1, maxNum);
+    }
+
     public static void main(String[] args) {
 
+        insertNum(4);
     }
 }
