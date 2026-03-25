4b6e238cab760a57e0957d399bf19bdf4fb36f3b
JoughnDough <4574790@myuwc.ac.za>
2026-03-25 14:21:03 +0200

    Added BST class to tryBST.java
    Also implemented the BST insert method

--- src/tryBST.java
+++ src/tryBST.java
@@ -15,6 +15,7 @@ public class tryBST {
         public tNode getLeft() {
             return left;
         }
+
         public void setLeft(tNode newLeftNode) {
             left = newLeftNode;
         }
@@ -22,6 +23,7 @@ public class tryBST {
         public tNode getRight() {
             return right;
         }
+
         public void setRight(tNode newRightNode) {
             right = newRightNode;
         }
@@ -29,12 +31,56 @@ public class tryBST {
         public tNode getParent() {
             return parent;
         }
+
         public void setParent(tNode newParentNode) {
             parent = newParentNode;
         }
 
         public int getKey() {
             return key;
+        }
+    }
+
+    public static class BST {
+        private tNode root;
+        private int length;
+
+        public BST() {
+            length = 0;
+            root = null;
+        }
+
+        //Adds a node to the tree
+        private void insert(tNode node, tNode currentNode) {
+            if (node.getKey() >= currentNode.getKey()) {
+                tNode rightNode = currentNode.getRight();
+
+                if (rightNode == null) {
+                    currentNode.setRight(node);
+                    node.setParent(currentNode);
+                } else
+                    insert(node, rightNode);
+            } else {
+                tNode leftNode = currentNode.getLeft();
+
+                if (leftNode == null) {
+                    currentNode.setLeft(node);
+                    node.setParent(currentNode);
+                } else
+                    insert(node, leftNode);
+            }
+        }
+
+        //Creates a new node from a given key and data before adding it into the tree
+        public void insert(int key, String data) {
+            tNode node = new tNode(key, data);
+
+            if (root == null)
+                root = node;
+            else
+                insert(node, root);
+
+            length++;
         }
     }
 
